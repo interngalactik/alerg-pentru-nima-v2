abfa767c424aed870422cba8192c08a265dfc92b
interngalactik <eduard.nistru@gmail.com>
2024-12-17 21:41:09 +0200

    fixed styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,10 @@ export const metadata: Metadata = {
     title: 'Alerg pentru Nima — Susține Sanctuarul Nima',
     description: 'Donează 2 euro / lună pentru hrana animalelor salvate de la abator sau exploatare — trimite NIMA prin SMS la 8845 iar eu voi alerga pentru fiecare mesaj în parte.',
     images: ['https://cdn.prod.website-files.com/675317edeb0cced92990c7d7/675b4db70569c8b35d0e9718_OPEN%20GRAPH.jpg'],
-  }
+  },
+  icons: {
+    icon: '/images/favicon.png',
+  },
 }
 
 export default function RootLayout({
